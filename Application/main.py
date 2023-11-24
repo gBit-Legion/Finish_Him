d225f318eb5f9baf71560e57186b63f54c433cd1
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-11-25 01:14:42 +0300

    update

--- Application/main.py
+++ Application/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from fastapi import FastAPI, UploadFile, routing
@@ -27,6 +28,21 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/video")
+async def video():
+    if not os.path.exists("./videos"):
+        os.mkdir("./videos")
+    list_file = os.listdir("./videos")
+
+    if len(list_file) == 0:
+        return {"error": "video not found"}
+    else:
+        dictionary = dict()
+        counter = 0
+        dictionary = {f"VideoURL{counter}": f"/get_url_video/{i}" for counter, i in enumerate(list_file, start=1)}
+        return dictionary
+
+
 @app.post("/archive_extract")
 async def unpacker(file: UploadFile):
     if not os.path.exists("./archive"):
@@ -48,8 +64,8 @@ static_router = routing.APIRouter(route_class=APIRoute)
 
 
 @static_router.get("/get_url_video/{filename}")
-def get_static_image(filename: str, video_name: str):
-    file_path = ""
+def get_static_image(filename: str):
+    file_path = "./videos" + "/" + filename
     return FileResponse(file_path)
 
 

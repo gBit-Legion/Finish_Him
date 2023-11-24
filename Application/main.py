d747df3689f60f92e8d26cbd6539f15d4de2a8e6
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-11-24 11:38:54 +0300

    update

--- Application/main.py
+++ Application/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, UploadFile, routing
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
+from fastapi.responses import FileResponse
 from prometheus_fastapi_instrumentator import Instrumentator
+
+from Connector.unpacker import extractor
 
 app = FastAPI(debug=True)
 
@@ -20,3 +26,31 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
+
+@app.post("/archive_extract")
+async def unpacker(file: UploadFile):
+    if not os.path.exists("./archive"):
+        os.makedirs("./archive")
+    file_path = "./archive" + "/" + file.filename
+
+    try:
+        with open(file_path, "wb") as f:
+
+            f.write(file.file.read())
+        extractor(file_path)
+
+    except Exception as e:
+        return {"directory_is_empty": e.args}
+    return {file.filename: "success"}
+
+
+static_router = routing.APIRouter(route_class=APIRoute)
+
+
+@static_router.get("/get_url_video/{filename}")
+def get_static_image(filename: str, video_name: str):
+    file_path = ""
+    return FileResponse(file_path)
+
+
+app.include_router(static_router)
